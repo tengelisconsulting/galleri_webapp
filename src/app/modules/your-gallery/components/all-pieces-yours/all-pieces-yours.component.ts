5878140aa5034526830f085e1894c3e76b51a810
Liam Tengelis <ltengelis@gmail.com>
2020-03-31 14:25:44 +0700

    layout of 'your pieces'

---
+++ src/app/modules/your-gallery/components/all-pieces-yours/all-pieces-yours.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-pieces-yours',
+  templateUrl: './all-pieces-yours.component.html',
+  styleUrls: ['./all-pieces-yours.component.css']
+})
+export class AllPiecesYoursComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/your-gallery/your-gallery.module.ts
+++ src/app/modules/your-gallery/your-gallery.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { YourGalleryRouteComponent } from './components/your-gallery-route/your-gallery-route.component';
 import { YourGalleryRoutingModule } from './your-gallery-routing.module';
+import { AllPiecesYoursComponent } from './components/all-pieces-yours/all-pieces-yours.component';
 
 
 @NgModule({
-  declarations: [YourGalleryRouteComponent],
+  declarations: [YourGalleryRouteComponent, AllPiecesYoursComponent],
   imports: [
     CommonModule,
     YourGalleryRoutingModule,

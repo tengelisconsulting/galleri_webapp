d061a2781b1dd8804529ebc743ccafb9e63e3243
Liam Tengelis <ltengelis@gmail.com>
2020-05-18 13:38:05 +0700

    prompt to create new user

--- src/app/modules/anon-gallery-view/anon-gallery-view.module.ts
+++ src/app/modules/anon-gallery-view/anon-gallery-view.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AnonGalleryViewRouteComponent } from './components/anon-gallery-view-route/anon-gallery-view-route.component';
 import { AnonGalleryViewRoutingModule } from './anon-gallery-view-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { MatButtonModule } from '@angular/material';
 
 
 
@@ -12,6 +13,7 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     AnonGalleryViewRoutingModule,
+    MatButtonModule,
     CommonModule,
     SharedModule,
   ]
--- src/app/modules/anon-gallery-view/components/anon-gallery-view-route/anon-gallery-view-route.component.ts
+++ src/app/modules/anon-gallery-view/components/anon-gallery-view-route/anon-gallery-view-route.component.ts
@@ -1,6 +1,9 @@
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { RouteComponent } from 'src/app/core/routing/RouteComponent';
 import { SessionService } from 'src/app/core/session.service';
+import { AppLoadService } from 'src/app/core/app-load.service';
+import { ModalService } from 'src/app/ui/modal.service';
+import { NewUserModalComponent } from 'src/app/modules/shared/create-user/new-user-modal/new-user-modal.component';
 
 
 @Component({
@@ -15,17 +18,33 @@ export class AnonGalleryViewRouteComponent extends RouteComponent<{
 }> {
 
   public sessionLoaded: boolean = false;
+  public expires: Date;
 
   constructor(
     cdr: ChangeDetectorRef,
+    private appLoad: AppLoadService,
+    private modalService: ModalService,
     private sessionService: SessionService,
   ) {
     super(cdr);
     this.appOnInit(() => {
       this.sessionService.enterSession(null, this.params.accessToken);
       this.sessionLoaded = true;
+      const claimsData = JSON.parse(atob(this.params.accessToken));
+      this.expires = new Date(claimsData.body.exp_ts * 1000);
       this.cdr.detectChanges();
     });
+  }
+
+  public async newUserModal(): Promise<void> {
+    const createRes = await this.modalService.showModal({
+      component: NewUserModalComponent,
+    });
+    if (createRes) {
+      await this.appLoad.startupFromLogin(
+        createRes.username, createRes.password
+      );
+    }
   }
 
 }
